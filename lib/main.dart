8fbed8eb08cb7aa855567bc15ed90c4b1922abcf
kechankrisna <ke.chankrisna168@gmail.com>
2021-03-07 16:38:11 +0700

    testing

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:file_selector/file_selector.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  XFile _file;
+
+  void _selectFile() async {
+    print("me");
+    final typeGroup = XTypeGroup(label: 'images', extensions: ['jpg', 'png']);
+    final file = await openFile(acceptedTypeGroups: [typeGroup]);
+    setState(() {
+      _file = file;
+    });
+    print(_file.readAsBytes());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _selectFile,
+        tooltip: 'upload file',
+        child: Icon(Icons.upload_file),
+      ),
+    );
+  }
+}
